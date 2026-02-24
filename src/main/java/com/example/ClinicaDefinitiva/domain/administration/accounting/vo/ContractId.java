95455910ac481871142cabd61847763a53e550a7
DavidSanclementeRamos <davidrsmos434@gmail.com>
2026-02-24 17:55:21 -0500

    refactor(accounting-dominio): migrar ContractId a record y simplificar validación
    - Se refactorizó ContractId para usar record en lugar de clase final
    - Se eliminó validación de valores <= 0, ya que el ID lo gestiona la base de datos
    - Se mantiene validación de null en el método estático of
    - Se ajustaron pruebas unitarias para reflejar la nueva regla:
      - Creación válida con valor no nulo
      - Excepción por valor nulo
      - Igualdad y diferencia entre instancias

--- src/main/java/com/example/ClinicaDefinitiva/domain/administration/accounting/vo/ContractId.java
+++ src/main/java/com/example/ClinicaDefinitiva/domain/administration/accounting/vo/ContractId.java
@@ -8,31 +8,18 @@ import com.example.ClinicaDefinitiva.domain.errors.catalog.errorAccounting.VoAcc
 import com.example.ClinicaDefinitiva.domain.errors.context.VOContext;
 import com.example.ClinicaDefinitiva.domain.exceptionsDomain.ValueObjectValidationException;
 
-public final class ContractId {
+public record  ContractId(Long getValue ) {
+    
 
-    private final Long value;
-
-    private ContractId(Long value) {
+    public static ContractId of(Long value) {
         if (value == null) {
             throw new ValueObjectValidationException(
                     VoAccountingError.ERR_CONTRACT_ID_NULL,
                     VOContext.ACCOUNTING
-            );
-        }
-        if (value <= 0) {
-            throw new ValueObjectValidationException(
-                    VoAccountingError.ERR_CONTRACT_ID_INVALID,
-                    VOContext.ACCOUNTING
-            );
-        }
-        this.value = value;
-    }
-
-    public static ContractId of(Long value) {
+            );}
         return new ContractId(value);
     }
 
-    public Long getValue() { return value; }
 }
 
 
