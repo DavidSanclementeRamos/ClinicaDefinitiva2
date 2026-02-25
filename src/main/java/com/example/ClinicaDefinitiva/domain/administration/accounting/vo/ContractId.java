0e017b2639dbd5e4a9d405facbd81f8952be3eb8
DavidSanclementeRamos <davidrsmos434@gmail.com>
2026-02-24 20:29:44 -0500

    fix(vo): revertir cambios posteriores en vo Document de accounting

--- src/main/java/com/example/ClinicaDefinitiva/domain/administration/accounting/vo/ContractId.java
+++ src/main/java/com/example/ClinicaDefinitiva/domain/administration/accounting/vo/ContractId.java
@@ -8,18 +8,31 @@ import com.example.ClinicaDefinitiva.domain.errors.catalog.errorAccounting.VoAcc
 import com.example.ClinicaDefinitiva.domain.errors.context.VOContext;
 import com.example.ClinicaDefinitiva.domain.exceptionsDomain.ValueObjectValidationException;
 
-public record  ContractId(Long getValue ) {
-    
+public final class ContractId {
 
-    public static ContractId of(Long value) {
+    private final Long value;
+
+    private ContractId(Long value) {
         if (value == null) {
             throw new ValueObjectValidationException(
                     VoAccountingError.ERR_CONTRACT_ID_NULL,
                     VOContext.ACCOUNTING
-            );}
+            );
+        }
+        if (value <= 0) {
+            throw new ValueObjectValidationException(
+                    VoAccountingError.ERR_CONTRACT_ID_INVALID,
+                    VOContext.ACCOUNTING
+            );
+        }
+        this.value = value;
+    }
+
+    public static ContractId of(Long value) {
         return new ContractId(value);
     }
 
+    public Long getValue() { return value; }
 }
 
 
