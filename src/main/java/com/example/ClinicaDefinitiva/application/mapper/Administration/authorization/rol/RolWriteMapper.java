0d2f2d67499d2f72acba81c78121155c440006a0
DavidSanclementeRamos <davidrsmos434@gmail.com>
2026-03-02 00:05:28 -0500

    refactor(authorization-application): simplificar RolWriteMapper y mantener solo transformación de permisos
    - Se eliminaron los métodos `fromCreateDto` y `fromCloneDto` de `RolWriteMapper`, ya que contenían lógica de creación/clonado del agregado `Rol`, responsabilidad que corresponde al Application Service y Domain Service.
    - Se mantuvo el método `toPermission`, que transforma un `PermissionDto` en un VO `Permission`, cumpliendo con el ADR-(Aplicación)-01.
    - Resultado: mapper más simple y alineado con la separación de responsabilidades, limitado a la transformación de DTOs en VOs.

--- src/main/java/com/example/ClinicaDefinitiva/application/mapper/Administration/authorization/rol/RolWriteMapper.java
+++ src/main/java/com/example/ClinicaDefinitiva/application/mapper/Administration/authorization/rol/RolWriteMapper.java
@@ -11,34 +11,13 @@ import com.example.ClinicaDefinitiva.domain.administration.authorization.vo.Reso
 import java.util.HashSet;
 
 public class RolWriteMapper {
-    public Permission toPermission(PermissionDto permissionDto) {
-        return null;
-    }
+    
 
-    public Rol fromCreateDto(CreateRolDto dto) {
-        return null;
-    }
-
-    /** public  Rol fromCreateDto(CreateRolDto dto) {
-        return Rol.createCustom(
-                RolEnum.valueOf(dto.rolEnum()),   // String → RolEnum
-                dto.description(),
-                new HashSet<>()                   // permisos vacíos al inicio
-        );
-
-    }
-
-    public Rol fromCloneDto(CreateRolDto dto, Rol rol){
-        return Rol.cloneRole(
-                rol,
-                dto.description()
-        );
-    }
     public Permission toPermission(PermissionDto dto) {
         return Permission.of(ResourceCatalog.of(
                 ResourceCatalog.BasicResource.valueOf(dto.resourceCode())),
                 ActionCatalog.of(ActionCatalog.BasicAction.valueOf(dto.actionCode()))); // crea VO con reglas de negocio
-    }*/
+    }
 
 }
 
